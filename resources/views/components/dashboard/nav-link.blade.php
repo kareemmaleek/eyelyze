9fdf14231b48c917bb7e6b5407e01d9eaf4a2d08
kareemmaleek <146902786+kareemmaleek@users.noreply.github.com>
2025-11-25 15:47:42 +0700

    updated minor

--- resources/views/components/dashboard/nav-link.blade.php
+++ resources/views/components/dashboard/nav-link.blade.php
@@ -1,7 +1,8 @@
 @props(['active' => false])
 
-<a {{ $attributes }} >
-    <div class="w-full p-2 px-5 my-1 flex gap-2 items-center {{ $active ? 'rounded-lg bg-lime-100' : 'hover:rounded-lg hover:bg-lime-100' }} ">
-       {{ $slot }}
+<a {{ $attributes }}>
+    <div
+        class="w-full p-2 px-5 my-1 flex gap-2 items-center {{ $active ? 'rounded-lg bg-lime-50' : 'hover:rounded-lg hover:bg-lime-50' }} ">
+        {{ $slot }}
     </div>
 </a>
--- resources/views/components/dashboard/navigation.blade.php
+++ resources/views/components/dashboard/navigation.blade.php
@@ -1,22 +1,22 @@
 <div class="w-full p-2 flex flex-col">
-   
+
 
     <div class="w-full px-2">
         <span class="uppercase text-[10px] font-bold text-gray-400 tracking-wider">general</span>
     </div>
 
     <x-dashboard.nav-link href="/" :active="request()->is('/')">
-         <x-heroicon-o-home class="w-6 h-6 text-lime-500" />
+        <x-heroicon-o-home class="w-6 h-6" />
         <span class="text-sm font-medium">Dashboard</span>
     </x-dashboard.nav-link>
 
     <x-dashboard.nav-link href="/track" :active="request()->is('track')">
-        <x-heroicon-o-map class="w-6 h-6 text-lime-500" />
+        <x-heroicon-o-map class="w-6 h-6 " />
         <span class="text-sm font-medium">Track/Find</span>
     </x-dashboard.nav-link>
 
     <x-dashboard.nav-link href="/report" :active="request()->is('report')">
-        <x-heroicon-o-document-chart-bar class="w-6 h-6 text-lime-500" />
+        <x-heroicon-o-document-chart-bar class="w-6 h-6 " />
         <span class="text-sm font-medium">Report</span>
     </x-dashboard.nav-link>
 
@@ -27,13 +27,13 @@
     </div>
 
     <x-dashboard.nav-link href="/report" :active="request()->is('devices')">
-        <x-heroicon-o-signal class="w-6 h-6 text-lime-500" />
+        <x-heroicon-o-signal class="w-6 h-6 " />
         <span class="text-sm font-medium">Devices</span>
     </x-dashboard.nav-link>
 
     @if (Auth::user()->role === 1)
         <x-dashboard.nav-link href="/users" :active="request()->is('users')">
-            <x-heroicon-o-users class="w-6 h-6 text-lime-500" />
+            <x-heroicon-o-users class="w-6 h-6 " />
             <span class="text-sm font-medium">Users</span>
         </x-dashboard.nav-link>
     @endif
@@ -45,11 +45,11 @@
         </div>
 
         <x-dashboard.nav-link href="/system-log" :active="request()->is('system-log')">
-            <x-heroicon-o-document-magnifying-glass class="w-6 h-6 text-lime-500" />
+            <x-heroicon-o-document-magnifying-glass class="w-6 h-6 " />
             <span class="text-sm font-medium">System Log</span>
         </x-dashboard.nav-link>
     @endif
-    
+
 
 
 </div>
